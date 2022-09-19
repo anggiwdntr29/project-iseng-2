f8b0d493007fdb5dfe8c6bf94a7abcdb14014904
Hokky Aryasta <hokkyaryasta@gmail.com>
2022-09-19 23:31:52 +0800

    responsive ser

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function getResult() {
     $('#result').html(`
         <h3 class="animate__animated animate__fadeInDown">YO NDAK TAU KOK TANYA SAYA</h3>
         <div class="gif-hehe animate__animated animate__fadeIn">
-            <img src="hehe.gif">
+            <img src="https://gifimage.net/wp-content/uploads/2017/07/funny-animal-gif-3.gif">
             <img src="https://media3.giphy.com/media/QUXYcgCwvCm4cKcrI3/giphy.gif">
         </div>
     `)
